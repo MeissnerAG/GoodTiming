b425f48daec5bd1cae703f3335c0973215999b1f
Burk <thomas.burk@meissner.eu>
2018-08-13 14:07:07 +0200

    Add project files.

---
+++ GoodTiming/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace GoodTiming
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.BtnGo = new System.Windows.Forms.Button();
+            this.txtSeconds = new System.Windows.Forms.TextBox();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblSeconds
+            // 
+            this.lblSeconds.AutoSize = true;
+            this.lblSeconds.Location = new System.Drawing.Point(13, 13);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(52, 13);
+            this.lblSeconds.TabIndex = 0;
+            this.lblSeconds.Text = "Seconds:";
+            // 
+            // BtnGo
+            // 
+            this.BtnGo.Location = new System.Drawing.Point(158, 8);
+            this.BtnGo.Name = "BtnGo";
+            this.BtnGo.Size = new System.Drawing.Size(44, 23);
+            this.BtnGo.TabIndex = 1;
+            this.BtnGo.Text = "Go!";
+            this.BtnGo.UseVisualStyleBackColor = true;
+            this.BtnGo.Click += new System.EventHandler(this.BtnGo_Click);
+            // 
+            // txtSeconds
+            // 
+            this.txtSeconds.Location = new System.Drawing.Point(71, 10);
+            this.txtSeconds.Name = "txtSeconds";
+            this.txtSeconds.Size = new System.Drawing.Size(81, 20);
+            this.txtSeconds.TabIndex = 2;
+            this.txtSeconds.Text = "180";
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(16, 34);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(186, 48);
+            this.lblTimer.TabIndex = 3;
+            this.lblTimer.Text = "0";
+            this.lblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Timer1
+            // 
+            this.Timer1.Interval = 1000;
+            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick_1);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 91);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.txtSeconds);
+            this.Controls.Add(this.BtnGo);
+            this.Controls.Add(this.lblSeconds);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Opacity = 0.75D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Good Timing";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSeconds;
+        private System.Windows.Forms.Button BtnGo;
+        private System.Windows.Forms.TextBox txtSeconds;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer Timer1;
+    }
+}
+
---
+++ GoodTiming/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using J3QQ4;
+
+namespace GoodTiming
+{
+    public partial class MainForm : Form
+    {
+        int timeLeft;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer1_Tick_1(object sender, EventArgs e)
+        {
+            if (timeLeft > 1)
+            {
+                timeLeft = timeLeft - 1;
+                lblTimer.Text = timeLeft.ToString();
+            }
+            else
+            {
+                Timer1.Stop();
+                lblTimer.Text = Emoji.Coffee;
+                lblTimer.ForeColor = Color.Gray;
+                System.Media.SystemSounds.Beep.Play();
+                BtnGo.Enabled = true;
+            }
+        }
+
+        private void BtnGo_Click(object sender, EventArgs e)
+        {
+            Int32.TryParse(txtSeconds.Text, out timeLeft);
+            lblTimer.Text = timeLeft.ToString();
+            lblTimer.ForeColor = Color.Black;
+            BtnGo.Enabled = false;
+            Timer1.Start();
+        }
+    }
+}
